9b6968fb630c0c5fe6ae6ede9cea8980b98b6355
Craig <cg37@outlook.com>
2026-05-26 11:55:00 +0800

    Dynamically set version from package.json
    Read package.json to set version dynamically.

--- bin/ncb.js
+++ bin/ncb.js
@@ -2,11 +2,19 @@
 import { program } from "commander";
 import chalk from "chalk";
 import { convertToCommand } from "../lib/index.js";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+// 获取当前文件的目录
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// 读取 package.json
+const pkg = JSON.parse(readFileSync(join(__dirname, "../package.json"), "utf8"));
 
 program
   .name("ncb")
   .description("将自然语言转换为Bash命令")
-  .version("1.0.0")
+  .version(pkg.version)
   .argument("<description>", "自然语言描述你想要的操作")
   .option("-y, --yes", "自动确认执行，不询问")
   .option("-d, --dry-run", "只显示生成的命令，不执行")
